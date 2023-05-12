0a7d64f36c27f85093ce3b97320d93fa7c4201ae
Zgjim27 <105733170+Zgjim270@users.noreply.github.com>
2023-05-13 00:26:46 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eCommerceApplication.Startup))]
+namespace eCommerceApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
